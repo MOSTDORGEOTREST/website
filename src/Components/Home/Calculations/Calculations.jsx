d5dec424b4915b3b4d9fe051c79354aacb65e9a8
Olesyahristenko <Olesyahristenkojob@yandex.ru>
2022-12-07 15:28:50 +0300

    Свернута трансляция, виды испытаний сделаны каруселью в мобильной версии, перенесена картинка в рассчетах

--- src/Components/Home/Calculations/Calculations.jsx
+++ src/Components/Home/Calculations/Calculations.jsx
@@ -1,35 +1,36 @@
-import React from 'react'
+import React from "react";
 
-import './Calculations.css'
+import "./Calculations.css";
 
-import welcome from './расчёты.png'
+import welcome from "./расчёты.png";
 
 export default function Calculations() {
-	return (
-		<>
-			<div className="home-calculations" id="Calculations">
-				<div className="home-blur"></div>
-				<div className="calculations__content">
-					<div className="calculations__main">
-						<div className="calculations__main-title">
-							<h1 className="calculations__main-title_main">Расчеты</h1>
-							<div className="calculations__main-title_sub">
-								В нашей лаборатории проводятся геотехнические расчеты грунтовых
-								сооружений, таких как насыпи, откосы, грунтовые дамбы и др., методом конечных элементов
-								с использованием нелинейных моделей грунтов, и аналитическими методами. Также в лаборатории проводится 
-								проектирование средств инженерной защиты (грунтовые стенки, анкерные системы, 
-								геосинтетические конструкции и др.).
-							</div>
-							<button className="main-title_btn" onClick={() => {}}>
-								Посмотреть цены
-							</button>
-						</div>
-						<div className="main_title_img">
-							<img src={welcome} alt="Calculations" />
-						</div>
-					</div>
-				</div>
-			</div>
-		</>
-	)
+  return (
+    <>
+      <div className="home-calculations" id="Calculations">
+        <div className="home-blur"></div>
+        <div className="calculations__content">
+          <div className="calculations__main">
+            <div className="calculations__main-title">
+              <h1 className="calculations__main-title_main">Расчеты</h1>
+              <div className="calculations__main-title_sub">
+                В нашей лаборатории проводятся геотехнические расчеты грунтовых
+                сооружений, таких как насыпи, откосы, грунтовые дамбы и др.,
+                методом конечных элементов с использованием нелинейных моделей
+                грунтов, и аналитическими методами. Также в лаборатории
+                проводится проектирование средств инженерной защиты (грунтовые
+                стенки, анкерные системы, геосинтетические конструкции и др.).
+              </div>
+              <div className="main_title_img calculations_title_img">
+                <img src={welcome} alt="Calculations" />
+              </div>
+              <button className="main-title_btn" onClick={() => {}}>
+                Посмотреть цены
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/Components/Home/Welcome/Welcome.jsx
+++ src/Components/Home/Welcome/Welcome.jsx
@@ -1,92 +1,257 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import Context from "../../../context";
+import welcome from "./imgs/welcome.png";
+import hill from "./imgs/rock.svg";
+import snow from "./imgs/snowflake.svg";
+import wave from "./imgs/wave.svg";
+import chem from "./imgs/bxs-flask.svg";
+import phys from "./imgs/scales-2-line.svg";
+import mech from "./imgs/mech.png";
+import "./Welcome.css";
 
-import Context from '../../../context'
-import welcome from './imgs/welcome.png'
-import hill from './imgs/rock.svg'
-import snow from './imgs/snowflake.svg'
-import wave from './imgs/wave.svg'
-import chem from './imgs/bxs-flask.svg'
-import phys from './imgs/scales-2-line.svg'
-import mech from './imgs/mech.png'
-
-import './Welcome.css'
+import useWindowDimensions from "../../windowResizeHook";
+import { useRef } from "react";
 
 export default function Welcome() {
-	const { setShowPrice } = useContext(Context)
+  const { setShowPrice } = useContext(Context);
 
-	function onShowPrice() {
-		const body = document.getElementById('body')
-		body.classList.add('body-hidden')
-		setShowPrice(true)
+  let widthSwith = useRef(false)
+
+  const { width } = useWindowDimensions();
+
+  function onShowPrice() {
+    const body = document.getElementById("body");
+    body.classList.add("body-hidden");
+    setShowPrice(true);
+  }
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+	if (width <= 545) { 
+		if (!widthSwith.current) {
+			widthSwith.current = true
+			setCurrentSlide(1);
+		}
+	} else {
+		widthSwith.current = false
 	}
+    
+  }, [width]);
 
-	return (
-		<>
-			<div className="home-welcome" id="welcome">
-				<div className="welcome__main">
-					<div className="welcome__main-title">
-						<h1 className="welcome__main-title_main">
-							Лидер в лабораторных
-							<br />
-							испытаниях грунтов
-						</h1>
-						<h3 className="welcome__main-title_sub">
-							В нашей лаборатории для выполнения испытаний грунтов мы используем
-							самое передовое оборудование отечественных и зарубежных
-							производителей
-						</h3>
-						<button className="main-title_btn" onClick={onShowPrice}>
-							Посмотреть цены
-						</button>
-					</div>
-					<div className="main_title_img">
-						<img src={welcome} alt="Welcome" />
-					</div>
-				</div>
-				<div className="welcome__labs">
-					<div className="labs_blur"></div>
-					<div className="labs-item">
-						<img src={phys} alt="" />
-						Определение
-						<br />
-						физических
-						<br />
-						свойств грунтов
-					</div>
-					<div className="labs-item">
-						<img src={mech} alt="" />
-						Определение
-						<br />
-						механических
-						<br />
-						свойств грунтов
-					</div>
-					<div className="labs-item">
-						<img src={wave} alt="" />
-						Динамическая
-						<br />
-						лаборатория
-					</div>
-					<div className="labs-item">
-						<img src={snow} alt="" />
-						Мерзлотная
-						<br />
-						лаборатория
-					</div>
-					<div className="labs-item">
-						<img src={hill} alt="" />
-						Скальная
-						<br />
-						лаборатория
-					</div>
-					<div className="labs-item">
-						<img src={chem} alt="" />
-						Химическая
-						<br />
-						лаборатория
-					</div>
-				</div>
-			</div>
-		</>
-	)
+  return (
+    <>
+      <div className="home-welcome" id="welcome">
+        <div className="welcome__main">
+          <div className="welcome__main-title">
+            <h1 className="welcome__main-title_main">
+              Лидер в лабораторных
+              <br />
+              испытаниях грунтов
+            </h1>
+            <h3 className="welcome__main-title_sub">
+              В нашей лаборатории для выполнения испытаний грунтов мы используем
+              самое передовое оборудование отечественных и зарубежных
+              производителей
+            </h3>
+            <button className="main-title_btn" onClick={onShowPrice}>
+              Посмотреть цены
+            </button>
+          </div>
+          <div className="main_title_img">
+            <img src={welcome} alt="Welcome" />
+          </div>
+        </div>
+        {width < 545 ? (
+          <>
+            <div className="welcome-objects">
+              <div
+                className="noclicker"
+                onClick={(event) => {
+                  event.stopPropagation();
+                }}
+              ></div>
+
+              <Carousel
+                className="customers-carousel"
+                showStatus={false}
+                showThumbs={false}
+                showArrows={false}
+                infiniteLoop={true}
+                autoPlay={true}
+                swipeable={false}
+                stopOnHover={false}
+                transitionTime={20000}
+                dynamicHeight={false}
+                showIndicators={false}
+                useKeyboardArrows={false}
+                interval={20000}
+                selectedItem={currentSlide}
+                onChange={(index) => setCurrentSlide(index)}
+              >
+                <div className="customers-object">
+                  <div className="table table-welcome">
+                    <div className="cart">
+                      <div className="labs-item">
+                        <img src={phys} alt="" />
+                        Определение
+                        <br />
+                        физических
+                        <br />
+                        свойств грунтов
+                      </div>
+                    </div>
+                    <div className="cart">
+                      <div className="labs-item">
+                        <img src={mech} alt="" />
+                        Определение
+                        <br />
+                        механических
+                        <br />
+                        свойств грунтов
+                      </div>
+                    </div>
+                    <div className="cart">
+                      <div className="labs-item">
+                        <img src={wave} alt="" />
+                        Динамическая
+                        <br />
+                        лаборатория
+                      </div>
+                    </div>
+                    <div className="cart">
+                      <div className="labs-item">
+                        <img src={snow} alt="" />
+                        Мерзлотная
+                        <br />
+                        лаборатория
+                      </div>
+                    </div>
+                    <div className="cart">
+                      <div className="labs-item">
+                        <img src={hill} alt="" />
+                        Скальная
+                        <br />
+                        лаборатория
+                      </div>
+                    </div>
+                    <div className="cart">
+                      <div className="labs-item">
+                        <img src={chem} alt="" />
+                        Химическая
+                        <br />
+                        лаборатория
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="customers-object">
+                  <div className="table table-welcome">
+                    <div className="cart">
+                      <div className="labs-item">
+                        <img src={phys} alt="" />
+                        Определение
+                        <br />
+                        физических
+                        <br />
+                        свойств грунтов
+                      </div>
+                    </div>
+                    <div className="cart">
+                      <div className="labs-item">
+                        <img src={mech} alt="" />
+                        Определение
+                        <br />
+                        механических
+                        <br />
+                        свойств грунтов
+                      </div>
+                    </div>
+                    <div className="cart">
+                      <div className="labs-item">
+                        <img src={wave} alt="" />
+                        Динамическая
+                        <br />
+                        лаборатория
+                      </div>
+                    </div>
+                    <div className="cart">
+                      <div className="labs-item">
+                        <img src={snow} alt="" />
+                        Мерзлотная
+                        <br />
+                        лаборатория
+                      </div>
+                    </div>
+                    <div className="cart">
+                      <div className="labs-item">
+                        <img src={hill} alt="" />
+                        Скальная
+                        <br />
+                        лаборатория
+                      </div>
+                    </div>
+                    <div className="cart">
+                      <div className="labs-item">
+                        <img src={chem} alt="" />
+                        Химическая
+                        <br />
+                        лаборатория
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Carousel>
+            </div>
+          </>
+        ) : (
+          <div className="welcome__labs">
+            <div className="labs_blur"></div>
+            <div className="labs-item">
+              <img src={phys} alt="" />
+              Определение
+              <br />
+              физических
+              <br />
+              свойств грунтов
+            </div>
+            <div className="labs-item">
+              <img src={mech} alt="" />
+              Определение
+              <br />
+              механических
+              <br />
+              свойств грунтов
+            </div>
+            <div className="labs-item">
+              <img src={wave} alt="" />
+              Динамическая
+              <br />
+              лаборатория
+            </div>
+            <div className="labs-item">
+              <img src={snow} alt="" />
+              Мерзлотная
+              <br />
+              лаборатория
+            </div>
+            <div className="labs-item">
+              <img src={hill} alt="" />
+              Скальная
+              <br />
+              лаборатория
+            </div>
+            <div className="labs-item">
+              <img src={chem} alt="" />
+              Химическая
+              <br />
+              лаборатория
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
