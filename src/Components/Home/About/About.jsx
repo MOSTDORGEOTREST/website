98ed79f3ecf68cdba14405dd71d8ed4ec1e7ac3c
Olesyahristenko <Olesyahristenkojob@yandex.ru>
2026-05-22 15:53:15 +0300

    feat: разрешительные

--- src/Components/Home/About/About.jsx
+++ src/Components/Home/About/About.jsx
@@ -13,7 +13,7 @@ import back_3d from './3d_back.png'
 // ДОКИ АО
 import AO_doc_1 from './AO_docs/Мосстройсертификация до 09.01.2028.pdf'
 import AO_doc_2 from './AO_docs/Росаккредитация-от-23.04.2020-г.pdf'
-import AO_doc_3 from './AO_docs/РОСС RU.0001.21АГ09 от 02.04.2026.pdf'
+import AO_doc_3 from './AO_docs/РОСС RU.0001.21АГ09 от 21.05.2026.pdf'
 import AO_doc_4 from './AO_docs/Сертификат ИСО СМК - до 24.09.2026.pdf'
 import AO_doc_5 from './AO_docs/СРО - Выписка из Реестра членов от 29.04.2026.pdf'
 
