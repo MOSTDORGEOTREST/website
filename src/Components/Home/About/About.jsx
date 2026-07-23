73088586e76e9391dc27bd1a0e2a38dcd15bf5c4
Gauss Lezhanrov <28481524+TriganTail@users.noreply.github.com>
2026-07-23 15:27:39 +0300

    временно обновляем АО
    надо вернуть ООО. по ошибке не там поменял

--- src/Components/Home/About/About.jsx
+++ src/Components/Home/About/About.jsx
@@ -14,7 +14,7 @@ import back_3d from './3d_back.png'
 import AO_doc_1 from './AO_docs/Мосстройсертификация до 09.01.2028.pdf'
 import AO_doc_2 from './AO_docs/Росаккредитация-от-23.04.2020-г.pdf'
 import AO_doc_3 from './AO_docs/РОСС RU.0001.21АГ09 от 03.07.2026.pdf'
-import AO_doc_4 from './AO_docs/Сертификат ИСО СМК - до 24.09.2026.pdf'
+import AO_doc_4 from './AO_docs/Сертификат ИСО СМК - до 21.06.2029.pdf'
 import AO_doc_5 from './AO_docs/СРО - Выписка из Реестра членов от 01.07.2026.pdf'
 
 // ДОКИ ООО
