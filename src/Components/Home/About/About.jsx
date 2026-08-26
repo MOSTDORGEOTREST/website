0f069179d57e15472bc4c42ab15c452b01c9a06b
Gauss Lezhanrov <28481524+TriganTail@users.noreply.github.com>
2026-08-26 13:04:57 +0300

    add pdf LAB in AO and sort doc (vers 1.0)

--- src/Components/Home/About/About.jsx
+++ src/Components/Home/About/About.jsx
@@ -16,6 +16,7 @@ import AO_doc_2 from './AO_docs/Росаккредитация-от-23.04.2020-�
 import AO_doc_3 from './AO_docs/РОСС RU.0001.21АГ09 от 03.08.2026.pdf'
 import AO_doc_4 from './AO_docs/Сертификат ИСО СМК - до 24.09.2026.pdf'
 import AO_doc_5 from './AO_docs/СРО - Выписка из Реестра членов от 03.08.2026.pdf'
+import AO_doc_6 from './AO_docs/РОССТАНДАРТ - Заключение о состоянии измерений в лаб.pdf'
 
 // ДОКИ ООО
 import OOO_doc_1 from './OOO_docs/Аккредитация-Промышленная-безопасность-до-03.09.2026-г.pdf'
@@ -138,6 +139,15 @@ export default function About() {
 								</div>
 								<div className="docs__card-list">
 									<a
+										href={AO_doc_6}
+										target="_blank"
+										rel="noopener noreferrer"
+										className="docs__card-list-item"
+									>
+										<img src={pdf} alt="pdf"></img>
+										<p>Заключение о состоянии измерений лаборатории</p>
+									</a>
+									<a
 										href={AO_doc_2}
 										target="_blank"
 										rel="noopener noreferrer"
@@ -204,15 +214,6 @@ export default function About() {
 								</div>
 								<div className="docs__card-list">
 									<a
-										href={OOO_doc_4}
-										target="_blank"
-										rel="noopener noreferrer"
-										className="docs__card-list-item"
-									>
-										<img src={pdf} alt="pdf"></img>
-										<p>Выписка из Реестра членов СРО</p>
-									</a>
-									<a
 										href={OOO_doc_1}
 										target="_blank"
 										rel="noopener noreferrer"
@@ -231,6 +232,15 @@ export default function About() {
 									>
 										<img src={pdf} alt="pdf"></img>
 										<p>Аттестат аккредитации АО “МОССТРОЙСЕРТИФИКАЦИЯ”</p>
+									</a>
+									<a
+										href={OOO_doc_4}
+										target="_blank"
+										rel="noopener noreferrer"
+										className="docs__card-list-item"
+									>
+										<img src={pdf} alt="pdf"></img>
+										<p>Выписка из Реестра членов СРО</p>
 									</a>
 									<a
 										href={OOO_doc_3}
