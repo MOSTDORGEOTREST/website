dedc231ed4e422653e0edd006b58172ca187c5f6
Gauss Lezhanrov <28481524+TriganTail@users.noreply.github.com>
2026-09-14 11:54:21 +0300

    update documents

--- src/Components/Home/About/About.jsx
+++ src/Components/Home/About/About.jsx
@@ -15,14 +15,14 @@ import AO_doc_1 from './AO_docs/Мосстройсертификация до 09
 import AO_doc_2 from './AO_docs/Росаккредитация-от-23.04.2020-г.pdf'
 import AO_doc_3 from './AO_docs/РОСС RU.0001.21АГ09 от 01.09.2026.pdf'
 import AO_doc_4 from './AO_docs/Сертификат ИСО СМК - до 24.09.2026.pdf'
-import AO_doc_5 from './AO_docs/СРО - Выписка из Реестра членов от 03.08.2026.pdf'
+import AO_doc_5 from './AO_docs/СРО - Выписка из Реестра членов от 03.09.2026.pdf'
 import AO_doc_6 from './AO_docs/РОССТАНДАРТ - Заключение о состоянии измерений в лаб.pdf'
 
 // ДОКИ ООО
 import OOO_doc_1 from './OOO_docs/РОССТАНДАРТ - Заключение о состоянии измерений в лаборатории.pdf'
 import OOO_doc_2 from './OOO_docs/Аккредитация - Мосстройсертификация - до 05.06.2028 г.pdf'
 import OOO_doc_3 from './OOO_docs/Сертификат ИСО - СМК до 21.06.2029.pdf'
-import OOO_doc_4 from './OOO_docs/СРО - Выписка из Реестра членов от 03.08.2026 г.pdf'
+import OOO_doc_4 from './OOO_docs/СРО - Выписка из Реестра членов от 03.09.2026 г.pdf'
 
 // Положение
 import mosgosexpert from './mosgosexpert.pdf'
